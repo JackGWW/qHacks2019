df36771089babcec3c0a81687122269208b4290b
Jack Williams <jackgww@gmail.com>
2019-02-02 19:49:30 -0500

    embed video stream in homepage

--- src/app.py
+++ src/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/video_stream')
-def stream_video():
+def video_stream():
 	return Response(video.main(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 

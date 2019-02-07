31cbe8f481ade970dbbaec0e91288b29f599af4d
Jack Williams <jackw667@gmail.com>
2019-02-06 23:30:35 -0500

    Clean up unused code in audio.py

--- src/audioStream.py
+++ src/audioStream.py
@@ -16,9 +16,9 @@ import threading
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"/Users/jnaylor/Documents/QHacks2019Local/gcpcredentialsnaylor.json"
-
-WORD_LIST = ['like', ' so', ' you know', 'basically', ' cuz', 'things', 'stuff', 'yeah']
+CUR_PATH = os.path.dirname(os.path.abspath(__file__))
+gcp_credential_path = os.path.join(CUR_PATH, '..', gcp_credential.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcp_credential_path
 
 
 class MicrophoneStream(object):
@@ -94,9 +94,9 @@ class AudioGenerator(threading.Thread):
         self.wpm = 0
         self.t = ''
         self.crutch = {}
+		self.crutch_words = ['like', ' so', ' you know', 'basically', ' cuz', 'things', 'stuff', 'yeah']
 
     start_stream = threading.Thread.start  # an alias for starting thread
-
 
     def get_wpm(self):
         return self.wpm
@@ -110,9 +110,7 @@ class AudioGenerator(threading.Thread):
             return self.t
 
     def run(self):
-
-
-            # See http://g.co/cloud/speech/docs/languages
+        # See http://g.co/cloud/speech/docs/languages
         # for a list of supported languages.
         language_code = 'en-US'  # a BCP-47 language tag
 
@@ -133,9 +131,7 @@ class AudioGenerator(threading.Thread):
             responses = client.streaming_recognize(streaming_config, requests)
 
             # Now, put the transcription responses to use.
-
             num_chars_printed = 0
-            WORD_COUNT = 0
             cur_bad_words = {}
             total_bad_words = {}
             start = time.time()
@@ -143,7 +139,7 @@ class AudioGenerator(threading.Thread):
             words = 0
             old_words = 0
 
-            for word in WORD_LIST:
+            for word in self.crutch_words:
                 cur_bad_words[word] = 0
                 total_bad_words[word] = 0
             try:
@@ -173,128 +169,31 @@ class AudioGenerator(threading.Thread):
                         # sys.stdout.write(transcript + overwrite_chars + '\r')
                         # sys.stdout.flush()
 
-
                         num_chars_printed = len(transcript)
-                        for word in WORD_LIST:
+                        for word in self.crutch_words:
                             cur_bad_words[word] = transcript.count(word) + total_bad_words[word]
 
                         words = len(transcript.split(' '))
                         total_time = time.time() - old_start
                         total_words = words + old_words
-
                         
-                        # with self.lock:
-                        self.wpm =  total_words / (total_time / 60)
-                        self.t = transcript
-                        self.crutch = cur_bad_words
+                        with self.lock:
+							self.wpm =  total_words / (total_time / 60)
+							self.t = transcript
+							self.crutch = cur_bad_words
 
                     # End of sentence
                     else:
                         old_words = words
                         old_start = start
                         start = time.time()
-                        total_bad_words = cur_bad_words
-
+						for word in self.crutch_words:
+							total_bad_words[word] = cur_bad_words[word]
             except:
                 pass
 
 
-def main():
-        # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    language_code = 'en-US'  # a BCP-47 language tag
-
-    client = speech.SpeechClient()
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=RATE,
-        language_code=language_code)
-    streaming_config = types.StreamingRecognitionConfig(
-        config=config,
-        interim_results=True)
-
-    with MicrophoneStream(RATE, CHUNK) as stream:
-        audio_generator = stream.generator()
-        requests = (types.StreamingRecognizeRequest(audio_content=content)
-                    for content in audio_generator)
-
-        responses = client.streaming_recognize(streaming_config, requests)
-
-        # Now, put the transcription responses to use.
-
-        num_chars_printed = 0
-        WORD_COUNT = 0
-        cur_bad_words = {}
-        total_bad_words = {}
-        start = time.time()
-        old_start = time.time()
-        words = 0
-        old_words = 0
-
-        for word in WORD_LIST:
-            cur_bad_words[word] = 0
-            total_bad_words[word] = 0
-        try:
-            for response in responses:
-                if not response.results:
-                    continue
-
-                # The `results` list is consecutive. For streaming, we only care about
-                # the first result being considered, since once it's `is_final`, it
-                # moves on to considering the next utterance.
-                result = response.results[0]
-                if not result.alternatives:
-                    continue
-
-                # Display the transcription of the top alternative.
-                transcript = result.alternatives[0].transcript
-
-                # Display interim results, but with a carriage return at the end of the
-                # line, so subsequent lines will overwrite them.
-                #
-                # If the previous result was longer than this one, we need to print
-                # some extra spaces to overwrite the previous result
-                overwrite_chars = ' ' * (num_chars_printed - len(transcript))
-
-                # In the middle on a sentence
-                if not result.is_final:
-                    # sys.stdout.write(transcript + overwrite_chars + '\r')
-                    # sys.stdout.flush()
-
-
-                    num_chars_printed = len(transcript)
-                    for word in WORD_LIST:
-                        cur_bad_words[word] = transcript.count(word) + total_bad_words[word]
-
-                    words = len(transcript.split(' '))
-                    total_time = time.time() - old_start
-                    total_words = words + old_words
-
-                    
-                    # with self.lock:
-                    wpm =  total_words / (total_time / 60)
-                    t = transcript
-                    crutch = cur_bad_words
-
-                    print("WPM: {}  Transcript:{}".format(wmp, transcript))
-                    print(crutch)
-
-                # End of sentence
-                else:
-                    old_words = words
-                    old_start = start
-                    start = time.time()
-                    total_bad_words = cur_bad_words
-
-        except:
-            pass
-
-
-
-
 if __name__ == '__main__':
-    main()
-    # ag = AudioGenerator()
-    # ag.start_stream()
-    # while True:
-    #     print("WPM: {}  Transcript:{}".format(ag.get_wpm, ag.get_transcript))
+    ag = AudioGenerator()
+    ag.run()
+    
